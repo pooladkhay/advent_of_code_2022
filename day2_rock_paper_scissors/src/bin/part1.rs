eaba3e765db34d22a270c563f1b391f6ab1dadea
Mohammad Javad Pooladkhay <m.pooladkhay@gmail.com>
2022-12-03 00:45:28 +0000

    update to more idiomatic rust

--- day2_rock_paper_scissors/src/bin/part1.rs
+++ day2_rock_paper_scissors/src/bin/part1.rs
@@ -12,63 +12,96 @@
 
 use std::{fs, vec};
 
-const ROCK: u32 = 1;
-const PAPER: u32 = 2;
-const SCISSORS: u32 = 3;
+const ROCK_SCORE: u32 = 1;
+const PAPER_SCORE: u32 = 2;
+const SCISSORS_SCORE: u32 = 3;
 
-const LOSE: u32 = 0;
-const DRAW: u32 = 3;
-const WIN: u32 = 6;
+const LOSE_SCORE: u32 = 0;
+const DRAW_SCORE: u32 = 3;
+const WIN_SCORE: u32 = 6;
+
+enum ElfHand {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum MyHand {
+    Rock,
+    Paper,
+    Scissors,
+}
 
 fn main() {
-    let input = get_strategy_guide();
+    let strategies = get_strategies("./src/input.txt");
 
     let mut total_score = 0;
 
-    for game in input {
-        let (elf_hand, my_hand) = game;
+    for strategy in strategies {
+        let (elf_hand, my_hand) = strategy;
         total_score += calc_score(elf_hand, my_hand);
     }
 
-    println!("total score: {}", total_score)
+    println!("Part 1 total score: {}", total_score)
 }
 
-fn calc_score(elf_hand: String, my_hand: String) -> u32 {
-    match elf_hand.as_str() {
-        "A" => match my_hand.as_str() {
-            "X" => DRAW + ROCK,
-            "Y" => WIN + PAPER,
-            "Z" => LOSE + SCISSORS,
-            _ => 0,
+fn calc_score(elf_hand: ElfHand, my_hand: MyHand) -> u32 {
+    match elf_hand {
+        ElfHand::Rock => match my_hand {
+            MyHand::Rock => DRAW_SCORE + ROCK_SCORE,
+            MyHand::Paper => WIN_SCORE + PAPER_SCORE,
+            MyHand::Scissors => LOSE_SCORE + SCISSORS_SCORE,
         },
-        "B" => match my_hand.as_str() {
-            "X" => LOSE + ROCK,
-            "Y" => DRAW + PAPER,
-            "Z" => WIN + SCISSORS,
-            _ => 0,
+        ElfHand::Paper => match my_hand {
+            MyHand::Rock => LOSE_SCORE + ROCK_SCORE,
+            MyHand::Paper => DRAW_SCORE + PAPER_SCORE,
+            MyHand::Scissors => WIN_SCORE + SCISSORS_SCORE,
         },
-        "C" => match my_hand.as_str() {
-            "X" => WIN + ROCK,
-            "Y" => LOSE + PAPER,
-            "Z" => DRAW + SCISSORS,
-            _ => 0,
+        ElfHand::Scissors => match my_hand {
+            MyHand::Rock => WIN_SCORE + ROCK_SCORE,
+            MyHand::Paper => LOSE_SCORE + PAPER_SCORE,
+            MyHand::Scissors => DRAW_SCORE + SCISSORS_SCORE,
         },
-        _ => 0,
     }
 }
 
-fn get_strategy_guide() -> Vec<(String, String)> {
-    let input = fs::read_to_string("./src/input.txt");
+// Panics if input file contains invalid strategies.
+fn get_strategies(input_file: &str) -> Vec<(ElfHand, MyHand)> {
+    let input = fs::read_to_string(input_file);
 
-    let mut strategies: Vec<(String, String)> = vec![];
+    let mut strategies: Vec<(ElfHand, MyHand)> = vec![];
 
     match input {
         Ok(data) => {
-            for line in data.lines() {
-                let hands: Vec<&str> = line.split(' ').collect();
-                let elf = hands.first().unwrap().to_owned().to_owned();
-                let me = hands.last().unwrap().to_owned().to_owned();
-                strategies.push((elf, me));
+            for (index, line) in data.lines().enumerate() {
+                let line = line.trim();
+                if !line.is_empty() {
+                    let strategy: Vec<&str> = line.split(' ').collect();
+
+                    let elf_hand = if let Some(elf_hand) = strategy.first() {
+                        match elf_hand.to_owned() {
+                            "A" => ElfHand::Rock,
+                            "B" => ElfHand::Paper,
+                            "C" => ElfHand::Scissors,
+                            _ => panic!("Invalid value found on strategy number: {}", index + 1),
+                        }
+                    } else {
+                        panic!("Invalid value found on strategy number: {}", index + 1)
+                    };
+
+                    let my_hand = if let Some(my_hand) = strategy.last() {
+                        match my_hand.to_owned() {
+                            "X" => MyHand::Rock,
+                            "Y" => MyHand::Paper,
+                            "Z" => MyHand::Scissors,
+                            _ => panic!("Invalid value found on strategy number: {}", index + 1),
+                        }
+                    } else {
+                        panic!("Invalid value found on strategy number: {}", index + 1)
+                    };
+
+                    strategies.push((elf_hand, my_hand));
+                }
             }
         }
         Err(err) => {
--- day2_rock_paper_scissors/src/bin/part2.rs
+++ day2_rock_paper_scissors/src/bin/part2.rs
@@ -8,63 +8,95 @@
 
 use std::{fs, vec};
 
-const ROCK: u32 = 1;
-const PAPER: u32 = 2;
-const SCISSORS: u32 = 3;
+const ROCK_SCORE: u32 = 1;
+const PAPER_SCORE: u32 = 2;
+const SCISSORS_SCORE: u32 = 3;
 
-const LOSE: u32 = 0;
-const DRAW: u32 = 3;
-const WIN: u32 = 6;
+const LOSE_SCORE: u32 = 0;
+const DRAW_SCORE: u32 = 3;
+const WIN_SCORE: u32 = 6;
+
+enum ElfHand {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum GameResult {
+    Lose,
+    Draw,
+    Win,
+}
 
 fn main() {
-    let input = get_strategy_guide();
-
+    let strategies = get_strategies("./src/input.txt");
     let mut total_score = 0;
 
-    for game in input {
-        let (elf_hand, my_hand) = game;
-        total_score += calc_score(elf_hand, my_hand);
+    for strategy in strategies {
+        let (elf_hand, game_result) = strategy;
+        total_score += calc_score(elf_hand, game_result);
     }
 
-    println!("total score: {}", total_score)
+    println!("Part 2 total score: {}", total_score)
 }
 
-fn calc_score(elf_hand: String, my_hand: String) -> u32 {
-    match elf_hand.as_str() {
-        "A" => match my_hand.as_str() {
-            "X" => LOSE + SCISSORS,
-            "Y" => DRAW + ROCK,
-            "Z" => WIN + PAPER,
-            _ => 0,
+fn calc_score(elf_hand: ElfHand, game_result: GameResult) -> u32 {
+    match elf_hand {
+        ElfHand::Rock => match game_result {
+            GameResult::Lose => LOSE_SCORE + SCISSORS_SCORE,
+            GameResult::Draw => DRAW_SCORE + ROCK_SCORE,
+            GameResult::Win => WIN_SCORE + PAPER_SCORE,
         },
-        "B" => match my_hand.as_str() {
-            "X" => LOSE + ROCK,
-            "Y" => DRAW + PAPER,
-            "Z" => WIN + SCISSORS,
-            _ => 0,
+        ElfHand::Paper => match game_result {
+            GameResult::Lose => LOSE_SCORE + ROCK_SCORE,
+            GameResult::Draw => DRAW_SCORE + PAPER_SCORE,
+            GameResult::Win => WIN_SCORE + SCISSORS_SCORE,
         },
-        "C" => match my_hand.as_str() {
-            "X" => LOSE + PAPER,
-            "Y" => DRAW + SCISSORS,
-            "Z" => WIN + ROCK,
-            _ => 0,
+        ElfHand::Scissors => match game_result {
+            GameResult::Lose => LOSE_SCORE + PAPER_SCORE,
+            GameResult::Draw => DRAW_SCORE + SCISSORS_SCORE,
+            GameResult::Win => WIN_SCORE + ROCK_SCORE,
         },
-        _ => 0,
     }
 }
 
-fn get_strategy_guide() -> Vec<(String, String)> {
-    let input = fs::read_to_string("./src/input.txt");
+// Panics if input file contains invalid strategies.
+fn get_strategies(input_file: &str) -> Vec<(ElfHand, GameResult)> {
+    let input = fs::read_to_string(input_file);
 
-    let mut strategies: Vec<(String, String)> = vec![];
+    let mut strategies: Vec<(ElfHand, GameResult)> = vec![];
 
     match input {
         Ok(data) => {
-            for line in data.lines() {
-                let hands: Vec<&str> = line.split(' ').collect();
-                let elf = hands.first().unwrap().to_owned().to_owned();
-                let me = hands.last().unwrap().to_owned().to_owned();
-                strategies.push((elf, me));
+            for (index, line) in data.lines().enumerate() {
+                let line = line.trim();
+                if !line.is_empty() {
+                    let strategy: Vec<&str> = line.split(' ').collect();
+
+                    let elf_hand = if let Some(elf_hand) = strategy.first() {
+                        match elf_hand.to_owned() {
+                            "A" => ElfHand::Rock,
+                            "B" => ElfHand::Paper,
+                            "C" => ElfHand::Scissors,
+                            _ => panic!("Invalid value found on strategy number: {}", index + 1),
+                        }
+                    } else {
+                        panic!("Invalid value found on strategy number: {}", index + 1)
+                    };
+
+                    let game_result = if let Some(game_result) = strategy.last() {
+                        match game_result.to_owned() {
+                            "X" => GameResult::Lose,
+                            "Y" => GameResult::Draw,
+                            "Z" => GameResult::Win,
+                            _ => panic!("Invalid value found on strategy number: {}", index + 1),
+                        }
+                    } else {
+                        panic!("Invalid value found on strategy number: {}", index + 1)
+                    };
+
+                    strategies.push((elf_hand, game_result));
+                }
             }
         }
         Err(err) => {
